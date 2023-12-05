745de8416b6e7e15d1bd736b3979ec60bca8ac63
Mike Rose <michaelrose151@gmail.com>
2023-12-05 15:56:17 -0500

    removed irrelevant line

--- src/js/config.js
+++ src/js/config.js
@@ -3,8 +3,3 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
-
-// export const API_SEARCH_URL =
-//   'https://forkify-api.herokuapp.com/api/v2/recipes?search=';
-// OR
-// 'https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>'
